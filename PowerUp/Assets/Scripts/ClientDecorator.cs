7dab2ba343b1650005160c8f569e9d86b1c0d905
d-pratt22 <156544542+d-pratt22@users.noreply.github.com>
2025-09-30 00:27:56 -0700

    IT WORKS!!!!!!!

--- PowerUp/Assets/Scripts/ClientDecorator.cs
+++ PowerUp/Assets/Scripts/ClientDecorator.cs
@@ -4,6 +4,7 @@ public class ClientDecorator : MonoBehaviour
 {
     private GunWeapon _gunWeapon;
     private bool _isAttached;
+    private int attachNum;
 
     void Start()
     {
@@ -14,9 +15,31 @@ public class ClientDecorator : MonoBehaviour
     {
         if (!_isAttached)
         {
-            if (GUILayout.Button("Put Attachments On"))
+            if (GUILayout.Button("Add Rate"))
             {
-                _gunWeapon.Decorate();
+                attachNum = 1;
+                _gunWeapon.Decorate(attachNum);
+                _isAttached = !_isAttached;
+            }
+
+            if (GUILayout.Button("Add Strength"))
+            {
+                attachNum = 2;
+                _gunWeapon.Decorate(attachNum);
+                _isAttached = !_isAttached;
+            }
+
+            if (GUILayout.Button("Add Amount"))
+            {
+                attachNum = 3;
+                _gunWeapon.Decorate(attachNum);
+                _isAttached = !_isAttached;
+            }
+
+            if (GUILayout.Button("Add Range"))
+            {
+                attachNum = 4;
+                _gunWeapon.Decorate(attachNum);
                 _isAttached = !_isAttached;
             }
         }
@@ -25,6 +48,7 @@ public class ClientDecorator : MonoBehaviour
         {
             if (GUILayout.Button("Reset Weapon"))
             {
+                attachNum = 0;
                 _gunWeapon.Reset();
                 _isAttached = !_isAttached;
             }
--- PowerUp/Assets/Scripts/GunWeapon.cs
+++ PowerUp/Assets/Scripts/GunWeapon.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class GunWeapon : MonoBehaviour
 {
@@ -9,11 +10,101 @@ public class GunWeapon : MonoBehaviour
     public WeaponAttachment attachment3;
     public WeaponAttachment attachment4;
 
-    public IWeapon _weapon;
+    public ParticleSystem baseConfig;
+    public ParticleSystem attachParticle1;
+    public ParticleSystem attachParticle2;
+    public ParticleSystem attachParticle3;
+    public ParticleSystem attachParticle4;
+
+    private IWeapon _weapon;
     private bool _isDecorated;
+    private bool _isFiring;
+    private int partAttach = 0;
+
 
     public void Start()
     {
         _weapon = new Weapon(weaponConfig);
+
+        Debug.Log("baseConfig: " + (baseConfig != null));
+        Debug.Log("attachParticle1: " + (attachParticle1 != null));
+
+    }
+
+    public void ToggleFire()
+    {
+        _isFiring = !_isFiring;
+
+        if (_isFiring )
+        {
+            StartCoroutine(FireWeapon());
+        }
+    }
+   
+    IEnumerator FireWeapon()
+    {
+        float fireRate = 20.0f / _weapon.Rate;
+
+        while (_isFiring)
+        {
+            yield return new WaitForSeconds(fireRate);
+            Debug.Log("Firing");
+
+            switch(partAttach)
+            {
+                case 1:
+                    attachParticle1.Play(true);
+                    break;
+                case 2:
+                    attachParticle2.Play(true);
+                    break;
+                case 3:
+                    attachParticle3.Play(true);
+                    break;
+                case 4:
+                    attachParticle4.Play(true);
+                    break;
+                default:
+                    //baseConfig.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+                    baseConfig.Play(true);
+                    break;  
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        _weapon = new Weapon(weaponConfig);
+        _isDecorated = !_isDecorated;
+        partAttach = 0;
+    }
+
+    public void Decorate(int attach)
+    {
+        if (attach == 1)
+        {
+            _weapon = new WeaponDecorator(_weapon, attachment1);
+            partAttach = 1;
+            Debug.Log("Attached 1");
+            Debug.Log(partAttach);
+        }
+
+        if (attach == 2)
+        {
+            _weapon = new WeaponDecorator(_weapon, attachment2);
+            partAttach = 2;
+        }
+
+        if (attach == 3)
+        {
+            _weapon = new WeaponDecorator(_weapon, attachment3);
+            partAttach = 3;
+        }
+
+        if (attach == 4)
+        {
+            _weapon = new WeaponDecorator(_weapon, attachment4);
+            partAttach = 4;
+        }
     }
 }
